34f3f6b3bb8b28b3c677cdad381b62693995a332
Abdelrahman Fareed <abdelrahmanfareed6@gmail.com>
2025-11-09 03:05:28 +0200

    update DataController.py
    add file validation check

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -6,7 +6,17 @@ class DataController(BaseController):
     def __init__(self):
         super().__init__()
 
+        self.MB_SIZE = 1024 * 1024
 
     def validate_file(self, file: UploadFile):
         """Check file type and size"""
-        pass
+        file_type = file.content_type
+        file_size = file.size
+
+        if file_type not in self.app_settings.ALLOWED_FILE_TYPE:
+            return False, "invalid type"
+        
+        if file_size > self.app_settings.MAX_FILE_SIZE_MB * self.MB_SIZE:
+            return False, "size is bigger than 25mb"
+        
+        return True, "File Is Valid"
--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
 
+    ALLOWED_FILE_TYPE: list
+    MAX_FILE_SIZE_MB: int
+
     class Config:
         env_file = ".env"
 
