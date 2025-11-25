00ef61f7aa8e7c5338d6212097755b5201436c9e
abdelrahmanfareed160 <abdelrahmanfareed6@gmail.com>
2025-11-25 19:34:27 +0200

    implement LLMInterface with abstract methods for model generation and embedding

---
+++ src/stores/LLM/LLMInterface.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+# abstract class
+# write the structure of the class without implmentation
+class LLMInterface(ABC):
+    
+    
+    @abstractmethod # this function must be implmented or used in child classes
+    def set_generation_model(self, model_id: str):
+        pass
+
+    @abstractmethod
+    def set_embedding_model(self, model_id: str):
+        pass
+
+    @abstractmethod
+    def generate_text(self, prompt: str, max_output_tokens: int,
+                      temprature: float = None):
+        pass
+
+    @abstractmethod
+    def embed_text(self, text: str, document_type: str):
+        # document type changes how the llm will embed the text
+        pass
+
+    @abstractmethod
+    def construct_prompt(self, prompt: str, role: str):
+        pass
